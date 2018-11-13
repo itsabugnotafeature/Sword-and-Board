15da0193734148da3add568669ac0ede778e850f
Max Sonderegger <max@maxs-macbook-pro.local>
2018-11-13 12:48:59 -0500

    Initial Commit

---
+++ Sword And Board/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Sword And Board
+//
+//  Created by Max Sonderegger on 11/13/18.
+//  Copyright © 2018 Max Sonderegger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
